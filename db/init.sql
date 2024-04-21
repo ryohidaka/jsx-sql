1d713ce716f1cbff914542cd2769abdc26741602
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-04-22 08:52:07 +0900

    feat: Add init.sql

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS my_table (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    status TEXT
+);
+
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
+INSERT INTO my_table (name, status) VALUES ('bob', 'paid');
